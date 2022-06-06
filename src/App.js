c85c688c5e76ab329677523944e199c112bef8f8
sachinviraj <74185781+Sviraj@users.noreply.github.com>
2022-06-06 23:10:17 +0530

    Added Project Handling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter} from "react-router-dom"; 
+import { Routes,Route } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Topbar from './Components/Topbar/Topbar';
+import Sidebar from './Components/Sidebar/Sidebar';
+import Home from './pages/Home/Home';
+import Projects from "./pages/Projects/Projects";
+import AddProject from './pages/AddProject/AddProject';
+import View from './pages/View/View';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+     <div className="App">
+         <Topbar/>
+         <div className="container">
+         <Sidebar/>
+         <div className="others">
+            <ToastContainer position="top-center"/>
+          <Routes>
+              
+              <Route exact path ='/' element={<Home/>} />
+              <Route path ='/project' element={<Projects/>} />
+              <Route path='/addProject' element={<AddProject/>} />
+              <Route path='/view/:id' element={<View/>} />
+               
+          </Routes>
+       
     </div>
+    </div>
+    </div>
+    </BrowserRouter>
+    
   );
 }
 
