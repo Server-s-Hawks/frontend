030f2cdc6cf33ef93ea2b6700f5f7c43202e9a93
Rashmika Tharindu <srtharindu@gmail.com>
2022-06-21 00:14:48 +0530

    new team and task

--- src/pages/Projects/ProjectNew.jsx
+++ src/pages/Projects/ProjectNew.jsx
@@ -15,31 +15,18 @@ const ProjectNew = () => {
         loadData();
     }, []);
 
-    const deleteContact = (project_ID) => {
-        if (
-            window.confirm("Are you sure that you wanted to delete that contact ?")
-
-        ) {
-            axios.delete(`http://localhost:4004/api/remove/${project_ID}`);
-            toast.success("Contact Deleted Successfully");
-            setTimeout(() => loadData(),500);
-
-        }
-    };
-
 
 
   return (
     <div style={{marginTop: "150px"}}>
-        {/* <Link to="/addProject">
-            <button className="btn btn-contact">Add Project</button>
-        </Link> */}
+       
+       
         <table className="styled-table">
             <thead>
                 <tr>
                     <th style={{textAlign: "center"}}>Project ID.</th>
                     <th style={{textAlign: "center"}}>Company.</th>
-                    <th style={{textAlign: "center"}}>Name.</th>
+                    <th style={{textAlign: "center"}}>Name</th>
                     <th style={{textAlign: "center"}}>Start_Date.</th>
                     <th style={{textAlign: "center"}}>Due_Date.</th>
                     <th style={{textAlign: "center"}}>Description.</th>
@@ -57,8 +44,8 @@ const ProjectNew = () => {
                             <td>{item.project_ID}</td>
                             <td>{item.project_company}</td>
                             <td>{item.project_name}</td>
-                            <td>{item.start_date}</td>
-                            <td>{item.due_date}</td>
+                            <td>{item.start_date.slice(0,10)}</td>
+                            <td>{item.due_date.slice(0,10)}</td>
                             <td>{item.description}</td>
                             <td>{item.supervisor_ID}</td>
                             <td>{item.manager_ID}</td>
